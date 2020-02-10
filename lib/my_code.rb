5a2e96201378f698cf1d6be8c7cbeca687ded7c4
isaac c <isaacgc0596@gmail.com>
2020-02-10 00:27:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,12 +10,25 @@ def map(array)
   n_array
 end
 
-def reduce(array, starting_point = 0)
-  value = starting_point
-  counter = 0 
+def reduce(array, starting_point = nil)
+  if starting_point
+    sum = starting_point
+    counter = 0
+  else
+    sum = array[0]
+    counter = 1
+  end
+  
   while counter < array.length do
-    value += yield(array[counter])
+    sum = yield(sum, array[counter])
     counter += 1
   end
-  value
+  sum
+  # value = starting_point
+  # counter = 0 
+  # while counter < array.length do
+  #   value += yield(array[counter])
+  #   counter += 1
+  # end
+  # value
 end
